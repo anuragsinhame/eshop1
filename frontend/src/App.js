2341ca33d3e33cafa9f1660af7ddc3ebad1fe945
Anurag Sinha <sinha.anurag101@gmail.com>
2021-06-20 11:59:51 +0530

    updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,11 +74,7 @@ function App() {
           <AdminRoute path="/admin" component={AdminHomeScreen}></AdminRoute>
           <Route path="/" component={HomeScreen} exact={true}></Route>
         </main>
-<<<<<<< HEAD
-        <Footer></Footer>
-=======
         {!isAdminPage && <Footer />}
->>>>>>> 1ccb449d33e3f28eebe63400e274bd8981282993
       </div>
     </BrowserRouter>
   );
